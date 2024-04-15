2f18e7ff6411d4967da6a8fbe9c5eaf4617be2bb
Chao Chen <chaochen@live.com>
2024-04-15 11:20:11 -0400

    feat: implement edit task modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ import AccordionHeaderSkeleton from './skeleton/accordion-header';
 import KanbanColumnSkeleton from './skeleton/kanban';
 import ProjectCardSkeleton from './skeleton/project-card';
 import LatestActivitiesSkeleton from './skeleton/latest-activities';
+
 import DashboardTotalCountCard from './home/total-count-card';
 import LatestActivities from './home/latest-activities';
 
@@ -19,3 +20,13 @@ export {
   DashboardTotalCountCard,
   LatestActivities,
 };
+
+export * from './tags/user-tag';
+export * from './text';
+export * from './accordion';
+export * from './tasks/form/description';
+export * from './tasks/form/due-date';
+export * from './tasks/form/stage';
+export * from './tasks/form/title';
+export * from './tasks/form/users';
+export * from './tasks/form/header';
