a07c48df7dee182e7674febd56754e7624d09410
Chao Chen <chaochen@live.com>
2024-04-15 07:41:43 -0400

    feat: implement create company modal

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import { authProvider, dataProvider, liveProvider } from './providers';
 import { Home, ForgotPassword, Login, Register, CompanyList } from './pages';
 import Layout from './components/layout';
 import { resources } from './config/resources';
+import Create from './pages/company/create';
 
 function App() {
   return (
@@ -57,7 +58,10 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
-                  <Route path="/companies" element={<CompanyList />} />
+                  <Route path="/companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                  </Route>
                 </Route>
               </Routes>
               <RefineKbar />
